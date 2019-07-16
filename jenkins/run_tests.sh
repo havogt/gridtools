a7b02c12683f718884574b0fb35d228bb2de0a32
Hannes Vogt <hannes@havogt.de>
2019-07-16 13:03:54 +0200

    move to build...

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -2,14 +2,15 @@
 
 source $(dirname "$0")/setup.sh
 
-./pyutils/driver.py -v -l $logfile build -b $build_type -p $real_type -g $grid_type -e $envfile -o build -i install -t install || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
+if [[ ! -z "${GT_CPP_BINDGEN_SOURCE_DIR}" ]]; then
+    echo "setting path for cpp_bindgen"
+    set_cpp_bindgen_source_dir="--cpp_bindgen-source-dir ${GT_CPP_BINDGEN_SOURCE_DIR}"
+fi
+
+./pyutils/driver.py -v -l $logfile build -b $build_type -p $real_type -g $grid_type -e $envfile -o build -i install -t install ${set_cpp_bindgen_source_dir} || { echo 'Build failed'; rm -rf $tmpdir; exit 1; }
 
 if [[ -z "${no_mpi}" ]]; then
     mpi_flag="-m"
 fi
 
-if [[ ! -z "${GT_CPP_BINDGEN_SOURCE_DIR}" ]]; then
-    set_cpp_bindgen_source_dir="--cpp_bindgen-source-dir ${GT_CPP_BINDGEN_SOURCE_DIR}"
-fi
-
-./build/pyutils/driver.py -v -l $logfile test $mpi_flag -b $set_cpp_bindgen_source_dir|| { echo 'Tests failed'; rm -rf $tmpdir; exit 2; }
+./build/pyutils/driver.py -v -l $logfile test $mpi_flag -b || { echo 'Tests failed'; rm -rf $tmpdir; exit 2; }
