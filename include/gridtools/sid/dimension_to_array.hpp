0dea9d3b0fbc07af23e241232b7a517faa4a4284
Hannes Vogt <hannes@havogt.de>
2023-03-30 13:31:28 +0000

    yet another version

--- include/gridtools/sid/dimension_to_array.hpp
+++ include/gridtools/sid/dimension_to_array.hpp
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include <functional>
 #include <type_traits>
 #include <utility>
 
@@ -90,155 +91,270 @@ namespace gridtools {
             struct init_helper;
             template <size_t... Is>
             struct init_helper<std::index_sequence<Is...>> {
-                template <class T, class Fun>
-                static GT_FUNCTION array<T, sizeof...(Is)> apply(Fun &&fun) {
-                    return {fun(Is)...};
+                template <class Fun>
+                static GT_FUNCTION auto apply(Fun &&fun) {
+                    return array{fun(Is)...};
                 }
             };
 
-            template <class Ptr, size_t N>
-            struct ptr_array {
-                array<Ptr, N> m_arr;
-                // OrigPtr m_orig_ptr;
-                // Stride m_stride;
+            template <class T, size_t N>
+            struct ref_array {
+                array<std::reference_wrapper<T>, N> m_arr;
 
-                // #if defined(__cpp_deduction_guides) and __cpp_deduction_guides >= 201907L
-                // #else
-                // no deduction guide for aggregate prior to c++20
-                // #endif
-
-                GT_FUNCTION constexpr auto const &operator[](size_t i) const { return *m_arr[i]; }
-                GT_FUNCTION auto &operator[](size_t i) { return *m_arr[i]; }
+                GT_FUNCTION constexpr T const &operator[](size_t i) const { return m_arr[i]; }
+                GT_FUNCTION T &operator[](size_t i) { return m_arr[i]; }
 
                 template <class TupleLike>
-                GT_FUNCTION ptr_array &operator=(TupleLike const &t) { // TODO enable only for tuple_like
+                GT_FUNCTION ref_array &operator=(TupleLike const &t) { // TODO enable only for tuple_like
                     assign_helper<std::make_index_sequence<tuple_util::size<TupleLike>::value>>::apply(
                         *this, t); // TODO simplify
                     return *this;
                 }
-
-                template <class StrideT, class Offset>
-                constexpr auto friend sid_shift(ptr_array &obj, StrideT const &stride, Offset const &o) {
-                    return ptr_array{tuple_util::transform(
-                        [stride, o](
-                            auto &&elem) { return sid::shifted(std::forward<decltype(elem)>(elem), stride, o); },
-                        obj.m_arr)};
-                    // return sid::shift(obj.m_orig_ptr, stride, o);
-                }
-
-                ptr_array &operator*() { return *this; }
-
-                ptr_array operator*() const { return *this; }
-
-                // template <class Arg>
-                // constexpr auto friend operator+(ptr_array const &obj, Arg &&arg) {
-                //     return ptr_array{tuple_util::transform(
-                //         [arg = std::forward<Arg>(arg)](auto &&elem) { return elem + std::forward<Arg>(arg); },
-                //         obj.m_arr)};
-                //     // return ptr{obj.m_orig_ptr + std::forward<Arg>(arg), obj.m_stride};
-                // }
-
-                // constexpr auto friend operator-(ptr_array const &lhs, ptr_array const &rhs) {
-                //     return ptr_array{
-                //         tuple_util::transform([](auto &&l, auto &&r) { return l - r; }, lhs.m_arr, rhs.m_arr)};
-                //     // return lhs.m_orig_ptr - rhs.m_orig_ptr;
-                // }
             };
 
-            template <class Ptr, size_t N, class Stride>
-            ptr_array<Ptr, N> init_ptr_array(Ptr orig_ptr, Stride const &stride) {
-                return {init_helper<std::make_index_sequence<N>>::template apply<Ptr>(
-                    [orig_ptr, stride](auto i) { return *shifted(orig_ptr, stride, i); })};
-            }
-
             template <size_t I, class Ptr, size_t N>
-            GT_FUNCTION auto &get(ptr_array<Ptr, N> &arr) noexcept {
+            GT_FUNCTION auto &get(ref_array<Ptr, N> &arr) noexcept {
                 // static_assert(I < D, "index is out of bounds");
                 return arr[I];
             }
 
             template <size_t I, class Ptr, size_t N>
-            GT_FUNCTION constexpr const auto &get(const ptr_array<Ptr, N> &arr) noexcept {
+            GT_FUNCTION constexpr const auto &get(const ref_array<Ptr, N> &arr) noexcept {
                 // static_assert(I < D, "index is out of bounds");
                 return arr[I];
             }
 
             template <size_t I, class Ptr, size_t N>
-            GT_FUNCTION constexpr auto &&get(ptr_array<Ptr, N> &&arr) noexcept {
+            GT_FUNCTION constexpr auto &&get(ref_array<Ptr, N> &&arr) noexcept {
                 // static_assert(I < D, "index is out of bounds");
                 return std::move(get<I>(arr));
             }
 
-            struct getter_ptr_array {
+            struct getter_ref_array {
                 template <size_t I, class Ptr, size_t N>
-                static GT_FUNCTION auto &get(ptr_array<Ptr, N> &arr) noexcept {
+                static GT_FUNCTION auto &get(ref_array<Ptr, N> &arr) noexcept {
                     // static_assert(I < D, "index is out of bounds");
                     return arr[I];
                 }
 
                 template <size_t I, class Ptr, size_t N>
-                static GT_FUNCTION constexpr const auto &get(const ptr_array<Ptr, N> &arr) noexcept {
+                static GT_FUNCTION constexpr const auto &get(const ref_array<Ptr, N> &arr) noexcept {
                     // static_assert(I < D, "index is out of bounds");
                     return arr[I];
                 }
 
                 template <size_t I, class Ptr, size_t N>
-                static GT_FUNCTION constexpr auto &&get(ptr_array<Ptr, N> &&arr) noexcept {
+                static GT_FUNCTION constexpr auto &&get(ref_array<Ptr, N> &&arr) noexcept {
                     // static_assert(I < D, "index is out of bounds");
                     return std::move(get<I>(arr));
                 }
             };
 
             template <class Ptr, size_t N>
-            getter_ptr_array tuple_getter(ptr_array<Ptr, N> const &);
+            getter_ref_array tuple_getter(ref_array<Ptr, N> const &);
 
-            template <class OrigPtr, class Stride, size_t N>
-            struct ptr {
-                ptr_array<OrigPtr, N> m_ptr_arr;
-                // OrigPtr m_orig_ptr;
-                // Stride const &m_stride;
-
-                GT_FUNCTION constexpr ptr(OrigPtr orig_ptr, Stride stride)
-                    : m_ptr_arr(init_ptr_array<OrigPtr, N>(orig_ptr, stride)) {}
-
-                GT_FUNCTION constexpr ptr(ptr_array<OrigPtr, N> ptr_arr) : m_ptr_arr{ptr_arr} {}
-                ptr() = default;
-
-                // auto operator*() { return init_ptr_array<OrigPtr, N>(m_orig_ptr, m_stride); }
-                auto operator*() const { return m_ptr_arr; }
-
-                // array_view<OrigPtr, Stride> operator*() const { return {m_orig_ptr, m_stride}; }
-
-                template <class Arg>
-                constexpr auto friend operator+(ptr const &obj, Arg &&arg) {
-                    return obj; // ptr{obj.m_ptr_arr + std::forward<Arg>(arg), obj.m_stride};
+            template <bool LValue>
+            struct ptr_array_deref_helper;
+            template <>
+            struct ptr_array_deref_helper<true> {
+                template <class Ptr, class Stride, size_t N>
+                static auto apply(Ptr &ptr, Stride const &stride) {
+                    return ref_array<std::remove_pointer_t<Ptr>, N>{
+                        init_helper<std::make_index_sequence<N>>::template apply<>(
+                            [&](auto i) { return std::ref(*sid::shifted(ptr, stride, i)); })};
                 }
-
-                constexpr auto friend operator-(ptr const &lhs, ptr const &rhs) {
-                    return lhs; // ptr{lhs.m_ptr_arr - rhs.m_ptr_arr};
-                }
-
-                template <class StrideT, class Offset>
-                constexpr auto friend sid_shift(ptr &obj, StrideT const &stride, Offset const &o) {
-                    return sid::shift(obj.m_ptr_arr, stride, o);
+            };
+            template <>
+            struct ptr_array_deref_helper<false> {
+                template <class Ptr, class Stride, size_t N>
+                static auto apply(Ptr const &ptr, Stride const &stride) {
+                    return init_helper<std::make_index_sequence<N>>::template apply<>(
+                        [&](auto i) { return *sid::shifted(ptr, stride, i); });
                 }
             };
 
-            template <class OrigPtrHolder, class Strides, size_t N>
+            template <class Ptr, class Stride, size_t N>
+            struct ptr_array {
+                Ptr m_ptr;
+                Stride m_stride;
+
+                // TODO
+                // #if defined(__cpp_deduction_guides) and __cpp_deduction_guides >= 201907L
+                // #else
+                // no deduction guide for aggregate prior to c++20
+                // #endif
+                ptr_array(Ptr ptr, Stride const &stride) : m_ptr(ptr), m_stride(stride) {}
+                ptr_array() = default;
+
+                template <class StrideT, class Offset>
+                constexpr auto friend sid_shift(ptr_array &obj, StrideT const &stride, Offset const &o) {
+                    return ptr_array{sid::shifted(obj.m_ptr, stride, o), obj.m_stride};
+                }
+
+                auto operator*() { // if the derefed ptr is an lvalue we return a array of references, otherwise an
+                                   // array of values
+                    return ptr_array_deref_helper<std::is_lvalue_reference<decltype(*(this->m_ptr))>::value>::
+                        template apply<Ptr, Stride, N>(this->m_ptr, this->m_stride);
+                }
+                auto operator*() const {
+                    // return tuple_util::transform([](auto &&elem) { return *elem; }, this->m_arr);
+                    return ptr_array_deref_helper<false>::template apply<Ptr, Stride, N>(this->m_ptr, this->m_stride);
+                }
+
+                template <class Arg>
+                constexpr auto friend operator+(ptr_array const &obj, Arg &&arg) {
+                    return ptr_array{obj.m_ptr + std::forward<Arg>(arg), obj.m_stride};
+                }
+
+                constexpr auto friend operator-(ptr_array const &lhs, ptr_array const &rhs) {
+                    return lhs.m_ptr - rhs.m_ptr;
+                }
+            };
+
+            // template <class Ptr, size_t N>
+            // struct ptr_array {
+            //     array<Ptr, N> m_arr;
+            //     // OrigPtr m_orig_ptr;
+            //     // Stride m_stride;
+
+            //     // #if defined(__cpp_deduction_guides) and __cpp_deduction_guides >= 201907L
+            //     // #else
+            //     // no deduction guide for aggregate prior to c++20
+            //     // #endif
+
+            //     // GT_FUNCTION constexpr auto const &operator[](size_t i) const { return *m_arr[i]; }
+            //     // GT_FUNCTION auto &operator[](size_t i) { return *m_arr[i]; }
+
+            //     template <class StrideT, class Offset>
+            //     constexpr auto friend sid_shift(ptr_array &obj, StrideT const &stride, Offset const &o) {
+            //         return ptr_array{tuple_util::transform(
+            //             [stride, o](
+            //                 auto &&elem) { return sid::shifted(std::forward<decltype(elem)>(elem), stride, o); },
+            //             obj.m_arr)};
+            //         // return sid::shift(obj.m_orig_ptr, stride, o);
+            //     }
+
+            //     auto operator*() {
+            //         // TODO: if the underlying thing is lvalue do this, else do the other
+            //         return ref_array<std::remove_pointer_t<Ptr>, N>{
+            //             init_helper<std::make_index_sequence<N>>::template apply<
+            //                 std::reference_wrapper<std::remove_pointer_t<Ptr>>>(
+            //                 [&](auto i) { return std::ref(*((this->m_arr)[i])); })};
+            //     }
+            //     auto operator*() const {
+            //         return tuple_util::transform([](auto &&elem) { return *elem; }, this->m_arr);
+            //     }
+            //     // ptr_array operator*() const { return *this; }
+
+            //     // template <class Arg>
+            //     // constexpr auto friend operator+(ptr_array const &obj, Arg &&arg) {
+            //     //     return ptr_array{tuple_util::transform(
+            //     //         [arg = std::forward<Arg>(arg)](auto &&elem) { return elem + std::forward<Arg>(arg); },
+            //     //         obj.m_arr)};
+            //     //     // return ptr{obj.m_orig_ptr + std::forward<Arg>(arg), obj.m_stride};
+            //     // }
+
+            //     // constexpr auto friend operator-(ptr_array const &lhs, ptr_array const &rhs) {
+            //     //     return ptr_array{
+            //     //         tuple_util::transform([](auto &&l, auto &&r) { return l - r; }, lhs.m_arr, rhs.m_arr)};
+            //     //     // return lhs.m_orig_ptr - rhs.m_orig_ptr;
+            //     // }
+            // };
+
+            // template <class Ptr, size_t N, class Stride>
+            // ptr_array<Ptr, N> init_ptr_array(Ptr orig_ptr, Stride const &stride) {
+            //     return {init_helper<std::make_index_sequence<N>>::template apply<Ptr>(
+            //         [orig_ptr, stride](auto i) { return &(*shifted(orig_ptr, stride, i)); })};
+            // }
+
+            // template <size_t I, class Ptr, size_t N>
+            // GT_FUNCTION auto &get(ptr_array<Ptr, N> &arr) noexcept {
+            //     // static_assert(I < D, "index is out of bounds");
+            //     return arr[I];
+            // }
+
+            // template <size_t I, class Ptr, size_t N>
+            // GT_FUNCTION constexpr const auto &get(const ptr_array<Ptr, N> &arr) noexcept {
+            //     // static_assert(I < D, "index is out of bounds");
+            //     return arr[I];
+            // }
+
+            // template <size_t I, class Ptr, size_t N>
+            // GT_FUNCTION constexpr auto &&get(ptr_array<Ptr, N> &&arr) noexcept {
+            //     // static_assert(I < D, "index is out of bounds");
+            //     return std::move(get<I>(arr));
+            // }
+
+            // struct getter_ptr_array {
+            //     template <size_t I, class Ptr, size_t N>
+            //     static GT_FUNCTION auto &get(ptr_array<Ptr, N> &arr) noexcept {
+            //         // static_assert(I < D, "index is out of bounds");
+            //         return arr[I];
+            //     }
+
+            //     template <size_t I, class Ptr, size_t N>
+            //     static GT_FUNCTION constexpr const auto &get(const ptr_array<Ptr, N> &arr) noexcept {
+            //         // static_assert(I < D, "index is out of bounds");
+            //         return arr[I];
+            //     }
+
+            //     template <size_t I, class Ptr, size_t N>
+            //     static GT_FUNCTION constexpr auto &&get(ptr_array<Ptr, N> &&arr) noexcept {
+            //         // static_assert(I < D, "index is out of bounds");
+            //         return std::move(get<I>(arr));
+            //     }
+            // };
+
+            // template <class Ptr, size_t N>
+            // getter_ptr_array tuple_getter(ptr_array<Ptr, N> const &);
+
+            // template <class OrigPtr, class Stride, size_t N>
+            // struct ptr {
+            //     ptr_array<OrigPtr, N> m_ptr_arr;
+            //     // OrigPtr m_orig_ptr;
+            //     // Stride const &m_stride;
+
+            //     GT_FUNCTION constexpr ptr(OrigPtr orig_ptr, Stride stride)
+            //         : m_ptr_arr(init_ptr_array<OrigPtr, N>(orig_ptr, stride)) {}
+
+            //     GT_FUNCTION constexpr ptr(ptr_array<OrigPtr, N> ptr_arr) : m_ptr_arr{ptr_arr} {}
+            //     ptr() = default;
+
+            //     // auto operator*() { return init_ptr_array<OrigPtr, N>(m_orig_ptr, m_stride); }
+            //     auto operator*() const { return m_ptr_arr; }
+
+            //     // array_view<OrigPtr, Stride> operator*() const { return {m_orig_ptr, m_stride}; }
+
+            //     template <class Arg>
+            //     constexpr auto friend operator+(ptr const &obj, Arg &&arg) {
+            //         return obj; // ptr{obj.m_ptr_arr + std::forward<Arg>(arg), obj.m_stride};
+            //     }
+
+            //     constexpr auto friend operator-(ptr const &lhs, ptr const &rhs) {
+            //         return lhs; // ptr{lhs.m_ptr_arr - rhs.m_ptr_arr};
+            //     }
+
+            //     template <class StrideT, class Offset>
+            //     constexpr auto friend sid_shift(ptr &obj, StrideT const &stride, Offset const &o) {
+            //         return sid::shift(obj.m_ptr_arr, stride, o);
+            //     }
+            // };
+
+            template <class OrigPtrHolder, class Stride, size_t N>
             struct ptr_holder {
                 OrigPtrHolder m_orig_ptr_holder;
-                Strides m_strides;
+                Stride m_stride;
 
                 auto operator()() const {
-                    return ptr<std::decay_t<decltype(std::declval<OrigPtrHolder>()())>, Strides, N>{
+                    return ptr_array<std::decay_t<decltype(std::declval<OrigPtrHolder>()())>, Stride, N>{
                         // return ptr<std::remove_cvref_t<decltype(std::declval<OrigPtrHolder>()())>, Strides>{
                         m_orig_ptr_holder(),
-                        m_strides};
+                        m_stride};
                 }
 
                 template <class Arg>
                 friend constexpr GT_FUNCTION ptr_holder operator+(ptr_holder const &obj, Arg &&offset) {
-                    return {obj.m_orig_ptr_holder + std::forward<Arg>(offset), obj.m_strides};
+                    return {obj.m_orig_ptr_holder + std::forward<Arg>(offset), obj.m_stride};
                 }
             };
 
--- tests/unit_tests/sid/test_sid_dimension_to_array.cpp
+++ tests/unit_tests/sid/test_sid_dimension_to_array.cpp
@@ -21,13 +21,45 @@
 namespace gridtools {
     namespace {
         using namespace literals;
+        // TEST(ptr_array, smoke) {
+        //     double data[15][42];
+
+        //     auto testee = *(sid::dimension_to_array_impl_::init_ptr_array<double *, 15>(
+        //         &data[0][0], std::integral_constant<int, 42>{}));
+        //     // ptr_array<double *, 15> testee(
+        //     // );
+
+        //     EXPECT_EQ(data[1][0], testee[1]);
+        //     EXPECT_EQ(&data[1][0], &testee[1]);
+        //     EXPECT_EQ(&data[2][0], &tuple_util::get<2>(testee));
+        //     using std::get;
+        //     EXPECT_EQ(&data[3][0], &get<3>(testee));
+
+        //     testee = array<double, 15>{};
+        // }
+        // TEST(ptr_array, nested) {
+        //     double data[15][42];
+
+        //     auto inner = sid::dimension_to_array_impl_::init_ptr_array<double *, 15>(
+        //         &data[0][0], std::integral_constant<int, 42>{});
+        //     auto testee = *(sid::dimension_to_array_impl_::init_ptr_array<decltype(inner), 42>(
+        //         inner, std::integral_constant<int, 1>{}));
+
+        //     EXPECT_TRUE(&testee);
+
+        //     GT_META_PRINT_TYPE(decltype(testee[0]))
+
+        //     // EXPECT_EQ(data[1][0], testee[0][1]);
+        //     // EXPECT_EQ(&data[1][0], &testee[0][1]);
+
+        //     // testee[0] = array<double, 15>{};
+        //     // testee = array<array<double, 15>, 42>{};
+        // }
         TEST(ptr_array, smoke) {
             double data[15][42];
 
-            auto testee = sid::dimension_to_array_impl_::init_ptr_array<double *, 15>(
+            auto testee = *sid::dimension_to_array_impl_::ptr_array<double *, std::integral_constant<int, 42>, 15>(
                 &data[0][0], std::integral_constant<int, 42>{});
-            // ptr_array<double *, 15> testee(
-            // );
 
             EXPECT_EQ(data[1][0], testee[1]);
             EXPECT_EQ(&data[1][0], &testee[1]);
@@ -40,10 +72,11 @@ namespace gridtools {
         TEST(ptr_array, nested) {
             double data[15][42];
 
-            auto inner = sid::dimension_to_array_impl_::init_ptr_array<double *, 15>(
+            auto inner = sid::dimension_to_array_impl_::ptr_array<double *, std::integral_constant<int, 42>, 15>(
                 &data[0][0], std::integral_constant<int, 42>{});
-            auto testee = sid::dimension_to_array_impl_::init_ptr_array<decltype(inner), 42>(
-                inner, std::integral_constant<int, 1>{});
+            auto testee =
+                *sid::dimension_to_array_impl_::ptr_array<decltype(inner), std::integral_constant<int, 1>, 42>(
+                    inner, std::integral_constant<int, 1>{});
 
             EXPECT_TRUE(&testee);
 
@@ -57,7 +90,7 @@ namespace gridtools {
         }
 
         TEST(dimension_to_array, smoke) {
-            double data[15][42];
+            double data[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
             auto testee = sid::dimension_to_array<gridtools::integral_constant<int, 0>, 15>(data);
 
             is_sid_separate<decltype(testee)>();
@@ -73,38 +106,38 @@ namespace gridtools {
 
             EXPECT_EQ(data[2][3], (*sid::shifted(ptr, tuple_util::get<0>(strides), 3))[2]);
 
-            (*sid::shifted(ptr, tuple_util::get<0>(strides), 4))[3] = 42;
-            EXPECT_EQ(42, data[3][4]);
+            (*sid::shifted(ptr, tuple_util::get<0>(strides), 2))[1] = 42;
+            EXPECT_EQ(42, data[1][2]);
         }
 
-        TEST(dimension_to_array, assignable_from_tuple_like) {
-            double data[2][5];
-            auto testee = sid::dimension_to_array<gridtools::integral_constant<int, 0>, 2>(data);
+        // TEST(dimension_to_array, assignable_from_tuple_like) {
+        //     double data[2][5];
+        //     auto testee = sid::dimension_to_array<gridtools::integral_constant<int, 0>, 2>(data);
 
-            static_assert(is_sid<decltype(testee)>::value);
+        //     static_assert(is_sid<decltype(testee)>::value);
 
-            auto ptr = sid::get_origin(testee)();
+        //     auto ptr = sid::get_origin(testee)();
 
-            *ptr = tuple(2., 3.);
-            EXPECT_EQ((*ptr)[0], 2.);
-            EXPECT_EQ((*ptr)[1], 3.);
-        }
+        //     *ptr = tuple(2., 3.);
+        //     EXPECT_EQ((*ptr)[0], 2.);
+        //     EXPECT_EQ((*ptr)[1], 3.);
+        // }
 
-        TEST(dimension_to_array, nested) {
-            double data[2][3];
-            auto testee = sid::dimension_to_array<integral_constant<int, 1>, 3>(
-                sid::dimension_to_array<integral_constant<int, 0>, 2>(data));
+        // TEST(dimension_to_array, nested) {
+        //     double data[2][3];
+        //     auto testee = sid::dimension_to_array<integral_constant<int, 1>, 3>(
+        //         sid::dimension_to_array<integral_constant<int, 0>, 2>(data));
 
-            static_assert(is_sid<decltype(testee)>::value);
+        //     static_assert(is_sid<decltype(testee)>::value);
 
-            auto ptr = sid::get_origin(testee)();
+        //     auto ptr = sid::get_origin(testee)();
 
-            auto derefed = *ptr;
-            auto outer = derefed[0];
-            GT_META_PRINT_TYPE(decltype(outer));
+        //     auto derefed = *ptr;
+        //     auto outer = derefed[0];
+        //     GT_META_PRINT_TYPE(decltype(outer));
 
-            // EXPECT_EQ(&data[1][2], &derefed[2][1]);
-        }
+        //     // EXPECT_EQ(&data[1][2], &derefed[2][1]);
+        // }
 
     } // namespace
 } // namespace gridtools
