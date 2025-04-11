1141a3489346087821b90eeec805ffc0cd2c7676
Hannes Vogt <hannes@havogt.de>
2025-04-11 15:12:24 +0200

    Update gtest to v1.16 (#1823)
    ... mainly for CMake 4.x support.

--- cmake/internal/FetchGoogletest.cmake
+++ cmake/internal/FetchGoogletest.cmake
@@ -12,7 +12,7 @@ function(fetch_googletest)
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG        release-1.11.0
+        GIT_TAG        v1.16.0
     )
     FetchContent_MakeAvailable(googletest)
 endfunction()
